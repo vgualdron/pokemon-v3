1548627d61fbbef468e979e2f1272c56c8d11cd9
vgualdron <victor.gualdron.r@gmail.com>
2025-01-11 21:01:46 -0500

    styles.css

--- .output/public/_nuxt/Ci6FHVVQ.js
+++
@@ -1,1 +0,0 @@
-class s{delay(e){return new Promise(o=>{setTimeout(()=>{o(2)},e)})}}export{s as C};
--- .output/public/_nuxt/Cmufyo7j.js
+++
@@ -1,1 +0,0 @@
-import{J as e}from"./DbJ4LzwP.js";const s=e("common",{state:()=>({loader:!1}),actions:{setLoader(o){this.loader=o}}});export{s as u};
