9413741e7f59d06aabf8b5cdef46abfb0fb8b522
vgualdron <victor.gualdron.r@gmail.com>
2025-01-12 12:47:27 -0500

    ignore 404 file

--- stores/common.js
+++ stores/common.js
@@ -1,13 +1,10 @@
-// stores/useLoader.js
 import { defineStore } from 'pinia';
 
 export const useCommonStore = defineStore('common', {
-  // Definir el estado
   state: () => ({
     loader: false
   }),
 
-  // Definir las acciones
   actions: {
     setLoader(value) {
       this.loader = value;
