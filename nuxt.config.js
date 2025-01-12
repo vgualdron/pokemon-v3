a454fe0c33f62bff9643df2969d0c039617982e4
vgualdron <victor.gualdron.r@gmail.com>
2025-01-11 21:42:35 -0500

    styles.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -90,6 +90,7 @@ export default defineNuxtConfig({
     },
     workbox: {
       globPatterns: ['**/*.{js,css,html,png,svg,ico}'],
+      globIgnores: ['400.html', '200.html'],
     },
     client: {
       installPrompt: true,
