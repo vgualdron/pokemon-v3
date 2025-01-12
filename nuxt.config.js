3b5e746c06c814f1487e292021f46f296837c13a
vgualdron <victor.gualdron.r@gmail.com>
2025-01-11 20:46:35 -0500

    styles.css

--- nuxt.config.js
+++ nuxt.config.js
@@ -57,7 +57,7 @@ export default defineNuxtConfig({
     'bootstrap/dist/css/bootstrap.css',
     'bootstrap-vue-next/dist/bootstrap-vue-next.css',
     'bootstrap-icons/font/bootstrap-icons.css',
-    '~/assets/css/styles.css',
+    '~/public/css/styles.css',
   ],
   components: true,
   modules: [
